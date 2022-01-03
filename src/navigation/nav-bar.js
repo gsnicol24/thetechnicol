7099600d5e4e3f3c2b1ddd8dac47a249aa299af4
Gavin Nicol <gnic24@gmail.com>
2022-01-03 09:06:31 -0500

    Modified nav bar a bit

--- src/navigation/nav-bar.js
+++ src/navigation/nav-bar.js
@@ -1,7 +1,8 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import Container from 'react-bootstrap/Container'
+
+import './nav-bar.scss'
 
 function NavigationBar() {
     return (
