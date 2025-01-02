635ed817fbb72b7a8cb3877a8d58436c196b4800
Gavin Nicol <gnic24@gmail.com>
2025-01-02 11:03:31 -0500

    Added other fields

--- src/gamedb/add-game-modal/game-details.tsx
+++ src/gamedb/add-game-modal/game-details.tsx
@@ -5,6 +5,9 @@ import { Container, Row, Col, Form } from "react-bootstrap";
 function GameDetails(props: { selectedId: string }) {
 
     const [imageUrl, setImageUrl] = useState<string | undefined>("")
+    const [gameName, setGameName] = useState<string | undefined>("")
+    const [minPlayers, setMinPlayers] = useState<number>(1)
+    const [maxPlayers, setMaxPlayers] = useState<number>(1)
 
 
     useEffect(() => {
@@ -25,32 +28,68 @@ function GameDetails(props: { selectedId: string }) {
         <div>
             <Form>
 
-                <Container fluid>
+
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <img
+                        style={{ maxWidth: "50%" }}
+                        src={imageUrl ? imageUrl : "question-mark.jpg"}
+                        onError={({ currentTarget }) => {
+                            if (currentTarget.src != "question-mark.jpg") {
+                                currentTarget.src = "question-mark.jpg";
+                            }
+                        }}
+                    />
+                </div>
+                <div>
                     <Row>
                         <Col>
-                            <div style={{ display: "flex", justifyContent: "center" }}>
-                                <img
-                                    style={{ maxWidth: "50%" }}
-                                    src={imageUrl ? imageUrl : "question-mark.jpg"}
-                                    onError={({ currentTarget }) => {
-                                        if (currentTarget.src != "question-mark.jpg") {
-                                            currentTarget.src = "question-mark.jpg";
-                                        }
-                                    }}
-                                />
-                            </div>
-                            <div>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Search"
-                                    className=" mr-sm-2"
-                                    onChange={(e) => setImageUrl(e.target.value)}
-                                    value={imageUrl}
-                                />
-                            </div>
+                            <Form.Label htmlFor="gameName">Game Name</Form.Label>
+                            <Form.Control
+                                id="gameName"
+                                type="text"
+                                className=" mr-sm-2"
+                                onChange={(e) => setGameName(e.target.value)}
+                                value={gameName}
+                            />
                         </Col>
                     </Row>
-                </Container>
+                    <Row>
+                        <Col>
+                            <Form.Label htmlFor="minPlayers">Min Players</Form.Label>
+                            <Form.Control
+                                id="minPlayers"
+                                type="number"
+                                min={1}
+                                className=" mr-sm-2"
+                                onChange={(e) => setMinPlayers(e.target.value as unknown as number)}
+                                value={minPlayers}
+                            />
+                        </Col>
+                        <Col>
+                            <Form.Label htmlFor="maxPlayers">Max Players</Form.Label>
+                            <Form.Control
+                                id="maxPlayers"
+                                type="number"
+                                min={1}
+                                className=" mr-sm-2"
+                                onChange={(e) => setMaxPlayers(e.target.value as unknown as number)}
+                                value={maxPlayers}
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Label htmlFor="gameImgSrc">Game Image URL</Form.Label>
+                            <Form.Control
+                                id="gameImgSrc"
+                                type="text"
+                                className=" mr-sm-2"
+                                onChange={(e) => setImageUrl(e.target.value)}
+                                value={imageUrl}
+                            />
+                        </Col>
+                    </Row>
+                </div>
             </Form>
         </div>
     )
