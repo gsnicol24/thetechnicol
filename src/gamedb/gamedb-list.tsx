05cb2ce554ded9637c18a4e3927c767d41c5e25c
Gavin Nicol <gnic24@gmail.com>
2025-01-06 12:08:03 -0500

    Removed objectfit

--- src/gamedb/gamedb-list.tsx
+++ src/gamedb/gamedb-list.tsx
@@ -40,7 +40,7 @@ function GameDBList() {
                                 <Col key={game.id} xs={12} sm={6} lg={4} xl={3}>
                                     <div style={{ display: "flex", justifyContent: "center", marginBottom: 16 }}>
                                         <Card style={{ minWidth: "90%", maxWidth: "90%" }}>
-                                            <Card.Img variant="top" src={game.img ?? "question-mark.jpg"} style={{ height: 250, objectFit: "cover" }} />
+                                            <Card.Img variant="top" src={game.img ?? "question-mark.jpg"} style={{ height: 250 }} />
                                             <Card.Header>
                                                 <span>
                                                     {game.name}
